579ff6f91714c5c96c5f511572f113754b1f4e7f
Boris Yevstyhneiev <molnerats@gmail.com>
2020-09-15 22:47:41 +0300

    initial project structure

---
+++ src/test/java/com/ss/greencity/EcoNewsOpenNewsTest.java
@@ -0,0 +1,22 @@
+package com.ss.greencity;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+/**
+ * Checks opening of news articles
+ */
+public class EcoNewsOpenNewsTest extends EcoNewsTestRunner {
+    @Test
+    public void openFirstNews() {
+        WebElement firstNews = driver.findElements(By.cssSelector("img.list-image-content")).get(0);
+        firstNews.click();
+        WebElement newsContent = driver.findElement(By.cssSelector("div.news-content"));
+        String text = newsContent.getText();
+        Assert.assertTrue("Verifying that news content is not empty", text.length() > 0);
+    }
+}
---
+++ src/test/java/com/ss/greencity/EcoNewsTestRunner.java
@@ -0,0 +1,47 @@
+package com.ss.greencity;
+
+import org.junit.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Default class for testing https://ita-social-projects.github.io/GreenCityClient/#/news
+ * Other classes for testing this page should extend this class
+ */
+public class EcoNewsTestRunner {
+    protected static WebDriver driver;
+    public EcoNewsTestRunner() {
+
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+        System.setProperty("webdriver.chrome.driver", "src/test/driver/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(1500, TimeUnit.MILLISECONDS);
+        driver.manage().timeouts().pageLoadTimeout(65, TimeUnit.SECONDS);
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        driver.close();
+        driver.quit();
+    }
+
+    @Before
+    public void setUp() {
+        driver.navigate().to("https://ita-social-projects.github.io/GreenCityClient/#/news");
+    }
+
+    @After
+    public void tearDown() {
+        //logout and so on
+    }
+
+
+}
