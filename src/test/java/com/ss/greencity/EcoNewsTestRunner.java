eee27de3a42202effaf9474419b27544d768090d
Udzhas <ura_dzhas@ukr.net>
2020-09-18 17:44:11 +0300

    signuptest1

--- src/test/java/com/ss/greencity/EcoNewsTestRunner.java
+++ src/test/java/com/ss/greencity/EcoNewsTestRunner.java
@@ -1,9 +1,12 @@
 package com.ss.greencity;
 
+import com.ss.greencity.pageelements.BaseElement;
 import com.ss.greencity.pageelements.Button;
 import com.ss.greencity.pageelements.Label;
+import com.ss.greencity.pageobjects.BasePageObject;
 import com.ss.greencity.pageobjects.EcoNewsListPO;
 import com.ss.greencity.pageobjects.NewsCardPO;
+import com.ss.greencity.pageobjects.SignUpPO;
 import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -90,6 +93,17 @@ public class EcoNewsTestRunner {
         }
         assertEquals("Count of correctly tagged news", allnews.size(), correctlyTaggedCount);
     }
-
-
+    @Test
+    public void signUpTest() {
+        SignUpPO signUpPO = new SignUpPO(driver);
+        String actualResult=signUpPO
+                .clickSignUpButton()
+                .setEmail("rg3.gg5")
+                .setUserName("vrtrtbr")
+                .setPassword("Aa123456_")
+                .setConfirmedPassword("Aa123456_")
+                .clickSignUpUserButton()
+                .alertEmailMessage();
+        Assert.assertEquals(actualResult," Please check that your e-mail address is indicated correctly ");
+    }
 }
