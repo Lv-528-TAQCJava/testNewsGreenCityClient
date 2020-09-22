6046fedd7c1438d1744180bcadc802aa8184a467
Ruslana Savuliak <savuliak9852@gmail.com>
2020-09-22 18:12:51 +0300

    Updated Header PO after review

--- src/main/java/com/ss/greencity/locators/HeaderLocators.java
+++ src/main/java/com/ss/greencity/locators/HeaderLocators.java
@@ -5,11 +5,11 @@ import org.openqa.selenium.By;
 public enum HeaderLocators implements Locator {
     HEADER(By.className("header")),
     LOGO(By.cssSelector("img[alt='logo']")),
-    NEWS(By.xpath("//div[@class='navigation-menu-left']/ul/li[1]")),
-    TIPS(By.xpath("//div[@class='navigation-menu-left']/ul/li[2]")),
-    PLACES(By.xpath("//div[@class='navigation-menu-left']/ul/li[3]")),
-    ABOUT(By.xpath("//div[@class='navigation-menu-left']/ul/li[4]")),
-    HABITS(By.xpath("//div[@class='navigation-menu-left']/ul/li[5]")),
+    NEWS(By.cssSelector((".navigation-menu-left a[href='#/news']"))),
+    TIPS(By.cssSelector((".navigation-menu-left a[href='#/tips']"))),
+    PLACES(By.cssSelector((".navigation-menu-left a[href='#/map']"))),
+    ABOUT(By.cssSelector((".navigation-menu-left a[href='#/about']"))),
+    HABITS(By.cssSelector((".navigation-menu-left a[href*='#/profile']"))),
     SEARCH(By.cssSelector("img[alt='search']")),
     LANGUAGE(By.cssSelector("div.switcher-wrapper > ul")), //language select dropdown menu
     SIGN_IN(By.cssSelector("li.sign-in-link")),
