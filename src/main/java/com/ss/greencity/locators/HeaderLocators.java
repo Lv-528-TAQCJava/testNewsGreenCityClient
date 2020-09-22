b8cd564a045f654f4298012bcd73b022681216bf
Ruslana Savuliak <savuliak9852@gmail.com>
2020-09-22 17:26:31 +0300

    Updated Header PO

--- src/main/java/com/ss/greencity/locators/HeaderLocators.java
+++ src/main/java/com/ss/greencity/locators/HeaderLocators.java
@@ -4,16 +4,17 @@ import org.openqa.selenium.By;
 
 public enum HeaderLocators implements Locator {
     HEADER(By.className("header")),
-    LOGO(By.cssSelector("")), //implement if necessary
-    NEWS(By.cssSelector("")), //implement if necessary
-    TIPS(By.cssSelector(".title-list.word-wrap p")), //implement if necessary
-    PLACES(By.cssSelector("")), //implement if necessary
-    ABOUT(By.cssSelector("")), //implement if necessary
-    HABITS(By.cssSelector("")), //implement if necessary
-    SEARCH(By.cssSelector("img[alt='search']")), //search button
+    LOGO(By.cssSelector("img[alt='logo']")),
+    NEWS(By.xpath("//div[@class='navigation-menu-left']/ul/li[1]")),
+    TIPS(By.xpath("//div[@class='navigation-menu-left']/ul/li[2]")),
+    PLACES(By.xpath("//div[@class='navigation-menu-left']/ul/li[3]")),
+    ABOUT(By.xpath("//div[@class='navigation-menu-left']/ul/li[4]")),
+    HABITS(By.xpath("//div[@class='navigation-menu-left']/ul/li[5]")),
+    SEARCH(By.cssSelector("img[alt='search']")),
     LANGUAGE(By.cssSelector("div.switcher-wrapper > ul")), //language select dropdown menu
     SIGN_IN(By.cssSelector("li.sign-in-link")),
-    SIGN_UP(By.cssSelector("li.sign-up-link > div")); //could replace one implemented in SignUpLocators
+    SIGN_UP(By.cssSelector("li.sign-up-link > div")), //could replace one implemented in SignUpLocators
+    USER_PROFILE(By.cssSelector("div#user-avatar-wrapper ul"));
 
     HeaderLocators(By path) {
         this.path = path;
--- src/main/java/com/ss/greencity/pageobjects/HeaderAnonymousPO.java
+++ src/main/java/com/ss/greencity/pageobjects/HeaderAnonymousPO.java
@@ -1,5 +1,6 @@
 package com.ss.greencity.pageobjects;
 
+import com.ss.greencity.locators.HeaderLocators;
 import com.ss.greencity.pageelements.Button;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,7 +15,18 @@ public class HeaderAnonymousPO extends HeaderPO {
     public HeaderAnonymousPO(WebDriver driver) {
         super(driver);
     }
+
     public HeaderAnonymousPO(WebElement element) {
         super(element);
+    }
+
+    public Button getSignIn() {
+        signIn = new Button(element.findElement(HeaderLocators.SIGN_IN.getPath()));
+        return signIn;
+    }
+
+    public Button getSignUp() {
+        signUp = new Button(element.findElement(HeaderLocators.SIGN_UP.getPath()));
+        return signUp;
     }
 }
