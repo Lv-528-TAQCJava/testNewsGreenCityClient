95806538d7584a8f4a778f39a01659490cdb5f67
Богдан Дзюбайло <banderol1024@gmail.com>
2020-09-24 15:00:30 +0300

    Created EditProfile Page object.

---
+++ src/main/java/com/ss/greencity/locators/CancelationPopupLocators.java
@@ -0,0 +1,20 @@
+package com.ss.greencity.locators;
+
+import org.openqa.selenium.By;
+
+public enum CancelationPopupLocators implements Locator {
+
+    CANCEL_BUTTON(By.cssSelector(".secondary-global-button")),
+    CONFIRM_BUTTON(By.cssSelector(".primary-global-button"));
+
+    CancelationPopupLocators(By path) {
+        this.path = path;
+    }
+
+    @Override
+    public By getPath() {
+        return path;
+    }
+
+    private By path;
+}
