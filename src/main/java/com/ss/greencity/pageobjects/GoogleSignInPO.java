3d3e5a3e4289d940686e30ad4ffd1f378634e7c1
ProstoLeleka <aliejua@gmail.com>
2020-09-28 17:16:09 +0300

    Refactored GoogleSignInPO

--- src/main/java/com/ss/greencity/pageobjects/GoogleSignInPO.java
+++ src/main/java/com/ss/greencity/pageobjects/GoogleSignInPO.java
@@ -3,40 +3,58 @@ package com.ss.greencity.pageobjects;
 import com.ss.greencity.pageelements.Button;
 import com.ss.greencity.pageelements.InputBox;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 import static com.ss.greencity.locators.GoogleSignInLocators.*;
 
 public class GoogleSignInPO extends BasePageObject {
+
+    private InputBox email_phone;
+    private InputBox password;
+    private Button emailNextBTN;
+    private Button passwordNextBTN;
 
     public GoogleSignInPO(WebDriver driver) {
         super(driver);
 
     }
 
+    public InputBox getEmail_Phone(){
+        email_phone= new InputBox(driver.findElement(EMAIL_PHONE_FIELD.getPath()));
+        return email_phone;
+    }
+
     public GoogleSignInPO setEmail_Phone(String value) {
-        InputBox email_phone= new InputBox(driver.findElement(EMAIL_PHONE_FIELD.getPath()));
-        email_phone.click();
-        email_phone.setData(value);
+       this.getEmail_Phone().click().setData(value);
         return this;
+    }
+
+    public InputBox getPassword(){
+        password= new InputBox(driver.findElement(PASSWORD_FIELD.getPath()));
+        return password;
     }
 
     public GoogleSignInPO setPassword(String value) {
-        InputBox password= new InputBox(driver.findElement(PASSWORD_FIELD.getPath()));
-        password.click();
-        password.setData(value);
-        return this;
+        this.getPassword().click().setData(value);
+       return this;
+    }
+
+    public Button getEmailNextBTN(){
+        emailNextBTN = new Button(driver.findElement(EMAIL_NEXT_BTN.getPath()));
+        return emailNextBTN;
     }
 
     public GoogleSignInPO clickEmailNextBTN(){
-        Button EmailNextBTN = new Button(driver.findElement(EMAIL_NEXT_BTN.getPath()));
-        EmailNextBTN.click();
+        this.getEmailNextBTN().click();
         return this;
     }
 
+    public Button getPasswordNextBtn(){
+      passwordNextBTN = new Button(driver.findElement(PASSWORD_NEXT_BTN.getPath()));
+      return passwordNextBTN;
+    }
+
     public GoogleSignInPO clickPasswordNextBTN(){
-        Button PasswordNextBTN = new Button(driver.findElement(PASSWORD_NEXT_BTN.getPath()));
-        PasswordNextBTN.click();
+        this.getPasswordNextBtn().click();
         return this;
     }
 
