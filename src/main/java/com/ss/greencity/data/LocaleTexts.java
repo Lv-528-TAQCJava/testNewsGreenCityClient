f6e424f7304e6c83e20a13c6d2f3a025ccb6066f
ilonaGrigorak <ilonochka185@gmail.com>
2020-09-29 19:31:33 +0300

    locales added

--- src/main/java/com/ss/greencity/data/LocaleTexts.java
+++ src/main/java/com/ss/greencity/data/LocaleTexts.java
@@ -1,2 +1,20 @@
-package com.ss.greencity.data;public class LocaleTexts {
+package com.ss.greencity.data;
+
+public enum LocaleTexts {
+    ECO_NEWS,
+    ECO_ADVICE,
+    ECO_MAP,
+    ABOUT_US,
+    MY_PROFILE,
+    SIGN_IN,
+    SIGN_UP,
+    MAIN_HEADER,
+    FILTER_BY,
+    ADS,
+    EVENTS,
+    NEWS,
+    EDUCATION,
+    INITIATIVES,
+    LIFEHACKS,
+    FOLLOW_US
 }
