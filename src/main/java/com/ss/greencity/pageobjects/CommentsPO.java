1ed8898eec9d8d4119af5c8cc49ecc8a03dce3bc
Boris Yevstyhneiev <molnerats@gmail.com>
2020-09-30 21:21:05 +0300

    replacing Thread.sleep with explicit waits in CommentsTest

--- src/main/java/com/ss/greencity/pageobjects/CommentsPO.java
+++ src/main/java/com/ss/greencity/pageobjects/CommentsPO.java
@@ -3,6 +3,7 @@ package com.ss.greencity.pageobjects;
 import com.ss.greencity.pageelements.Button;
 import com.ss.greencity.pageelements.InputBox;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import static com.ss.greencity.locators.CommentsLocators.*;
 
@@ -13,6 +14,8 @@ public class CommentsPO extends BasePageObject {
     }
 
     public InputBox getCommentsInputbox() {
+        waitsSwitcher.setExplicitWait(ExpectedConditions
+                .visibilityOfElementLocated(COMMENTS_INPUTBOX.getPath()));
         return new InputBox(driver.findElement(COMMENTS_INPUTBOX.getPath()));
     }
 
@@ -21,7 +24,20 @@ public class CommentsPO extends BasePageObject {
     }
 
     public String getLastCommentText() {
+        waitsSwitcher.setExplicitWait(ExpectedConditions
+                .visibilityOfElementLocated(LAST_COMMENT.getPath()));
         return driver.findElement(LAST_COMMENT.getPath()).getText();
+    }
+
+    /**
+     * Clicks comment button and waits until it's disabled
+     * (at this time the new comment is supposed to be published)
+     */
+    public CommentsPO clickCommentButton() {
+        getCommentButton().click();
+        waitsSwitcher.setExplicitWait(ExpectedConditions.not(ExpectedConditions
+                .elementToBeClickable(COMMENT_BUTTON.getPath())));
+        return this;
     }
 }
 
--- src/test/java/com/ss/greencity/CommentsTest.java
+++ src/test/java/com/ss/greencity/CommentsTest.java
@@ -1,8 +1,10 @@
 package com.ss.greencity;
 
 import com.ss.greencity.pageobjects.*;
+import com.ss.greencity.util.WaitsSwitcher;
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -20,11 +22,7 @@ public class CommentsTest extends EcoNewsTestRunner {
 
         System.out.println("Go to Eco News, open the first news card");
         HeaderSignedInComponent HeaderSignedInComponent = new HeaderSignedInComponent(driver);
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
         HeaderSignedInComponent.getNews().click();
         EcoNewsListPO newsListPO = new EcoNewsListPO(driver);
         newsListPO.getFirstNewsCard().getClickableArea().click();
@@ -34,12 +32,13 @@ public class CommentsTest extends EcoNewsTestRunner {
         String timeNow = new SimpleDateFormat("yyyyMMdd_HHmmss")
                 .format(Calendar.getInstance().getTime());
         commentsPO.getCommentsInputbox().setData(timeNow);
-        commentsPO.getCommentButton().click();
-        try {
-            Thread.sleep(15000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        commentsPO.getCommentButton().click();
+//        try {
+//            Thread.sleep(15000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        commentsPO.clickCommentButton(); //the wait is implemented here
         commentsPO.refreshPage();
         String actualResult = commentsPO.getLastCommentText();
         Assert.assertEquals(timeNow, actualResult);
@@ -49,18 +48,10 @@ public class CommentsTest extends EcoNewsTestRunner {
         HeaderSignedInComponent.signOut();
 
         System.out.println("Go to Eco News, open the first news card");
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
         HeaderAnonymousComponent HeaderAnonymousComponent = new HeaderAnonymousComponent(driver);
         HeaderAnonymousComponent.getNews().click();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
         newsListPO.getFirstNewsCard().getClickableArea().click();
         actualResult = commentsPO.getLastCommentText();
         Assert.assertEquals(timeNow, actualResult);
